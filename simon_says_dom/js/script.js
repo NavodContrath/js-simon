cf84ed0d173c438823a00bbb42808cf32ab57db2
NavodContrath <navod.contrath@gmail.com>
2025-02-20 14:57:43 +0100

    add varElements

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,26 @@
+/* 
+📌Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+📌NOTA:
+non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+📌BONUS:
+Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
+Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
+*/
+/*
+📌SUMMARY:
+- create var linked to html elements
+
+*/
+const countdownEl = document.getElementById("countdown")
+const instructionsEl = document.getElementById("instructions")
+const numberListEl = document.getElementById("number-list")
+const formEl = document.getElementById("answer-form")
+const inputGroupEl = document.getElementById("input-group")
+const formControlEl = document.querySelector(".form-control")
+const submitBtnEl = document.querySelector(".btn")
+const messageEl = document.getElementById("message")
+
